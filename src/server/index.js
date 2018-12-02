139142e1385cb91ab69e29e32f943a38a5a118fb
Huy Nguyen <>
2018-12-02 08:17:47 +0700

    update naming, test tx ropsten

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,19 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const api = require('./routes');
 
-const { SECRET } = require('./helper/constant')
-
+const { 
+	secret, 
+	es256Private, 
+	es256Public, 
+	ethAddress, 
+	ropstenId, 
+	ethBaseGasLimit, 
+	ethGasPricePerByte,
+	ropstenInfuraApi,
+	burnAddress
+} = require('./helper/constant')
+const EthereumTx = require('ethereumjs-tx')
+const Web3 = require('web3');
 const PORT = 8080;
 
 app.use(bodyParser.json());
@@ -17,7 +28,7 @@ app.use(cors())
 
 app.use(function(req, res, next) {
 	if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Beaer') {
-		jwt.verify(req.headers.authorization.split(' ')[1], SECRET, function(err, decode) {
+		jwt.verify(req.headers.authorization.split(' ')[1], secret, function(err, decode) {
 			if (err) req.user = undefined;
 			req.user = decode;
 			next();
@@ -33,7 +44,44 @@ app.use(express.static(join(__dirname, 'dist')));
 
 
 app.use('/api', api);
+app.get('/tx', (req, res) => {
+	const web3 = new Web3(new Web3.providers.HttpProvider(ropstenInfuraApi));
 
+	const obj = {
+		a: "a demo"
+	}
+
+	const strObj = JSON.stringify(obj)
+	const buffer = Buffer.from(strObj)
+	const encoded = '0x' + buffer.toString('hex')
+	web3.eth.getTransactionCount(ethAddress)
+		.then((count) => {
+			return {
+				nonce: web3.utils.toHex(count),
+				gasPrice: web3.utils.toHex(55e9), 
+				gasLimit: web3.utils.toHex(ethBaseGasLimit + ethGasPricePerByte * buffer.length),
+				to: burnAddress, 
+				value: '0x00', 
+				data: encoded,
+				chainId: ropstenId
+			  }
+		})
+		.then((txData) => {
+			console.log(txData)
+			const tx = new EthereumTx(txData)
+			tx.sign(Buffer.from('165884bba6f397852ac47428750091baebd33f8195cbb0b22206561228835806', 'hex'))
+			return '0x' + tx.serialize().toString('hex')
+		})
+		.then((signedTx) => web3.eth.sendSignedTransaction(signedTx))
+		.then((resTx) => {
+			console.log(resTx.transactionHash)
+			res.send(resTx)
+		})
+		.catch((err) => {
+			console.log(err)
+			res.send("errr")
+		})
+});
 app.get('*', (req, res) => {
 	res.sendFile(join(__dirname, '../../public/index.html'));
 });
--- src/server/user/index.js
+++ src/server/user/index.js
@@ -4,7 +4,7 @@
  * File Created: Monday, 22nd October 2018 11:05:51 am
  * Author: huynguyen (qhquanghuy96@gmail.com)
  * -----
- * Last Modified: Friday, 2nd November 2018 10:43:29 am
+ * Last Modified: Sunday, 2nd December 2018 1:51:32 am
  * Modified By: huynguyen (qhquanghuy96@gmail.com)
  * -----
  */
@@ -19,7 +19,7 @@ const { idWithLog } = require('./../helper/functions')
 const jwt = require('jsonwebtoken');
 const { createUser, findUserByEmail, getUserProfileById } = require('./user-dao')
 const { ServerError } = require('./../helper/server-error')
-const {SECRET} = require('./../helper/constant')
+const {secret} = require('./../helper/constant')
 const { prop } = require('ramda')
 
 router.get("/", (req, res) => {
@@ -41,7 +41,7 @@ router.get("/", (req, res) => {
 
 router.post("/signup", (req, res) => {
     createUser(req.body)
-        .then(user => res.send({token: jwt.sign({id: user.id, email: user.email, role: user.role }, SECRET) }))
+        .then(user => res.send({token: jwt.sign({id: user.id, email: user.email, role: user.role }, secret) }))
         .catch(err => { console.log(err); res.status(500).send({ message: "Server error" })})
 })
 
@@ -49,7 +49,7 @@ router.post("/signin", (req, res) => {
     findUserByEmail(req.body.email, req.body.password)
         .then(([rows]) => {
             if(rows[0]) {
-                res.send({token: jwt.sign({id: rows[0].id, email: rows[0].email, role: rows[0].role }, SECRET) })
+                res.send({token: jwt.sign({id: rows[0].id, email: rows[0].email, role: rows[0].role }, secret) })
             } else {
                 res.status(401).send({message: "email or password is incorrect!"})
             }
